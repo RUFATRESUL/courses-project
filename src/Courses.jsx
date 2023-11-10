0a820bf7afc246b1e9904668ba1b200b6b11c708
RUFAT RESUL <rufatresul997@gmail.com>
2023-11-10 23:39:47 +0400

    styleUpdate-courses

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -1,27 +1,31 @@
 import { useState } from 'react';
 // import Course from './Course'
+import {FaChevronLeft,FaChevronRight} from 'react-icons/fa'
 
 function Courses({courses,deleteCourse}) {
-    const [index, setIndex] = useState(1)
-   const {title,price,content} = courses[index];
+    const [index, setIndex] = useState(0);
+    const{title,price,content} = courses[index]
+    // console.log(courses);
     return ( <div className="courseList">
-        <div><h2>Courses :</h2></div>
+        <div><h2>Kurslarim :</h2></div>
+        <div className='cardDiv'>
+            <button className='iconButton'>
+                <FaChevronLeft/>
+            </button>
         <div className="card">
         <div className="courseCard">
         <h3 className="courseTitle"><i>{title}</i></h3>
         <h5 className="coursePrice">{price}AZN</h5>
         </div>
         <p className="courseContent">{content}</p>
-        {/* <button className="deleteButton" onClick={()=>removeCourse(id)}>Delete</button> */}
+       
     </div>
-        
-        {/* {courses.map((course) =>{
-            return(
-            <Course key={course.id} {...course} removeCourse={deleteCourse}/>
-            )
-        })} */}
+            <button className='iconButton'>
+                <FaChevronRight/>
+            </button>
+      
         </div>
-    );
+    </div> );
 }
 
 export default Courses;
