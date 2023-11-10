89a2c90b84e7d7ea66018d8c7221cfb50e06d9eb
RUFAT RESUL <rufatresul997@gmail.com>
2023-11-10 17:15:22 +0400

    updateCourse

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,8 +1,8 @@
+
 function Course({id,content,title,price,removeCourse}) {
     
     return ( <div className="card">
         <div className="courseCard">
-
         <h3 className="courseTitle"><i>{title}</i></h3>
         <h5 className="coursePrice">{price}AZN</h5>
         </div>
--- src/Courses.jsx
+++ src/Courses.jsx
@@ -1,16 +1,27 @@
-import Course from './Course'
+import { useState } from 'react';
+// import Course from './Course'
+
 function Courses({courses,deleteCourse}) {
-    // console.log(courses);
+    const [index, setIndex] = useState(1)
+   const {title,price,content} = courses[index];
     return ( <div className="courseList">
         <div><h2>Courses :</h2></div>
-        <div className='cardDiv'>
-        {courses.map((course) =>{
+        <div className="card">
+        <div className="courseCard">
+        <h3 className="courseTitle"><i>{title}</i></h3>
+        <h5 className="coursePrice">{price}AZN</h5>
+        </div>
+        <p className="courseContent">{content}</p>
+        {/* <button className="deleteButton" onClick={()=>removeCourse(id)}>Delete</button> */}
+    </div>
+        
+        {/* {courses.map((course) =>{
             return(
             <Course key={course.id} {...course} removeCourse={deleteCourse}/>
             )
-        })}
+        })} */}
         </div>
-    </div> );
+    );
 }
 
 export default Courses;
